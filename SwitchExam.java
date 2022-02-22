2704e6535f30e69d21854edbfa4bab22e74d0cb4
aaa286 <84839619+aaa286@users.noreply.github.com>
2022-02-22 19:15:44 +0900

    Create SwitchExam.java

---
+++ SwitchExam.java
@@ -0,0 +1,30 @@
+package javaStudy;
+
+public class SwitchExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//switch, case, default, break
+		
+		int value = 2;
+		
+		switch(value) {
+			case 1 : 
+				System.out.println("1"); break;
+			case 2 : 
+				System.out.println("2"); break;
+			case 3 : 
+				System.out.println("3"); break;
+			default : 
+				System.out.println("그 외 다른 숫자");
+		} //end of switch
+		String str = "A";
+		switch(str) {
+			case "A" : 
+				System.out.println("A"); break;
+			case "B" : 
+				System.out.println("B"); break;
+		} //end of switch
+	} //end of main
+
+} //end of class
