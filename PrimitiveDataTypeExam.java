17fff83f2a983f65482efd864c7af35fcd7b9255
aaa286 <84839619+aaa286@users.noreply.github.com>
2022-02-22 19:15:19 +0900

    Create PrimitiveDataTypeExam.java

---
+++ PrimitiveDataTypeExam.java
@@ -0,0 +1,21 @@
+package javaStudy;
+
+public class PrimitiveDataTypeExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean isFun = false;
+		System.out.println(isFun);
+		
+		char c = 'f';
+		
+		int x = 59;
+		
+		long big = 345455433l;
+		
+		float f = 32.4f;
+		
+		double d = 3434343.5;
+	} //end of main
+
+} //end of class
