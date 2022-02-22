bf315154a54be7234135effb400b48840523d6ee
aaa286 <84839619+aaa286@users.noreply.github.com>
2022-02-22 19:09:52 +0900

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,23 @@
+package javaStudy;
+
+public class HelloWorld {
+//	프로그램의 시작점.
+/**
+ * 문서화 주석
+ * @param args
+ */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		//System.out.println("test"); 행 단위 주석
+		/* System.out.println("1");
+		 * System.out.println("2");
+		 * System.out.println("3"); 블럭 단위 주석
+		 */
+		System.out.println("단축키 Ctrl+/");
+		System.out.println("단축키 Ctrl+/");
+		System.out.println("단축키 Ctrl+/");
+		System.out.println("단축키 Ctrl+/");
+	} //end of main
+
+} //end of class
